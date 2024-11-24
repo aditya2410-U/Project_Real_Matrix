3143a733bb856daa0332a4af1838c1638a96e817
Aditya <adityasharma1946@gmail.com>
2024-11-24 22:00:02 +0530

    added compiler using Piston API

--- matrix-app/app/codeeditor/components/EditorComponent.tsx
+++ matrix-app/app/codeeditor/components/EditorComponent.tsx
@@ -10,12 +10,18 @@ import Editor from "@monaco-editor/react";
 import Button from "@mui/material/Button";
 import PlayArrow from "@mui/icons-material/PlayArrow";
 import { codeSnippets, languageOptions } from "@/config/config";
+import { compileCode } from "@/actions/compile";
+import { Loader, TriangleAlert } from "lucide-react";
+import toast from "react-hot-toast";
 
 export default function EditorComponent() {
   const [sourceCode, setSourceCode] = useState(
     codeSnippets[languageOptions[0].language]
   );
   const [languageOption, setLanguageOption] = useState(languageOptions[0]);
+  const [loading, setLoading] = useState(false);
+  const [output, setOutput] = useState<string[]>([]);
+  const [err, setErr] = useState(false);
   const editorRef = useRef(null);
 
   function handleEditorDidMount(editor: any) {
@@ -34,6 +40,30 @@ export default function EditorComponent() {
     setSourceCode(codeSnippets[value.language]);
   }
 
+  async function executeCode() {
+    setLoading(true);
+    const requestData = {
+      language: languageOption.language,
+      version: languageOption.version,
+      files: [
+        {
+          content: sourceCode,
+        },
+      ],
+    };
+    try {
+      const result = await compileCode(requestData);
+      setOutput(result.run.output.split("\n"));
+      setLoading(false);
+      setErr(false);
+      toast.success("Compiled Successfully");
+    } catch (error) {
+      setErr(true);
+      setLoading(false);
+      toast.error("Failed to compile the Code");
+    }
+  }
+  
   return (
     <div className="h-[695px] bg-slate-900 rounded-3xl shadow-2xl py-6 px-8 overflow-hidden">
       <div className="flex items-center justify-between py-3">
@@ -41,7 +71,10 @@ export default function EditorComponent() {
           Codex
         </h2>
         <div className="flex">
-          <SelectLanguage onSelect={onSelect} selectedLanguageOption={languageOption} />
+          <SelectLanguage
+            onSelect={onSelect}
+            selectedLanguageOption={languageOption}
+          />
         </div>
       </div>
       {/* This is the resizable component */}
@@ -64,21 +97,47 @@ export default function EditorComponent() {
           <ResizableHandle className="bg-black w-2" />
 
           <ResizablePanel defaultSize={50} minSize={35}>
-            <div className="p-4">
-              <div className="flex items-center justify-between bg-slate-900 px-6 py-2">
+          <div className="space-y-3 bg-slate-900 min-h-screen">
+          <div className="flex items-center justify-between  bg-slate-950 px-6 py-2">
                 <h2 className="text-white text-xl font-semibold">Output</h2>
-                <Button
-                  variant="contained"
-                  className="bg-purple-800 hover:bg-purple-500 text-white"
-                  startIcon={<PlayArrow />}
-                >
-                  Run
-                </Button>
+                {loading ? (
+                  <Button
+                    disabled
+                    className="bg-slate-200 hover:bg-purple-700 text-dark-900"
+                  >
+                    <Loader className="w-4 h-4 mr-2 animate-spin" />
+                    <span>Running please wait...</span>
+                  </Button>
+                ) : (
+                  <Button
+                    onClick={executeCode}
+                    variant="contained"
+                    className="bg-purple-800 hover:bg-purple-500 text-white"
+                    startIcon={<PlayArrow />}
+                  >
+                    Run
+                  </Button>
+                )}
               </div>
-              <div className="h-full bg-slate-900 mt-4 p-6 rounded-lg">
-                <h2 className="text-white text-xl font-semibold text-center">
-                  Hello World
-                </h2>
+              <div className=" px-6 space-y-2">
+                {err ? (
+                  <div className="flex items-center space-x-2 text-red-500 border border-red-600 px-6 py-6">
+                    <TriangleAlert className="w-5 h-5 mr-2 flex-shrink-0" />
+                    <p className="text-xs">
+                      Failed to Compile the Code , Please try again !
+                    </p>
+                  </div>
+                ) : (
+                  <>
+                    {output.map((item) => {
+                      return (
+                        <p className="text-sm text-green-500"  key={item}>
+                          {item}
+                        </p>
+                      );
+                    })}
+                  </>
+                )}
               </div>
             </div>
           </ResizablePanel>
