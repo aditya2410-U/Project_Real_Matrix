cb97f65fbb451b689fa159835237747992c75d90
Yuvraj Singh Nain <yuvrajsinghnain03@gmail.com>
2024-12-08 22:46:42 +0530

    Doing nextjs backend for login and signup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -53,6 +53,27 @@
 //   );
 // }
 
+// 'use client';
+// import { AuthProvider } from '@/context/AuthContext';
+// import Login from './auth/login/page';
+
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>
+//         <AuthProvider>
+//           {children}
+//           <Login />
+//         </AuthProvider>
+//       </body>
+//     </html>
+//   );
+// }
+
 "use client";
 
 // import LoginForm from '../components/LoginForm';
